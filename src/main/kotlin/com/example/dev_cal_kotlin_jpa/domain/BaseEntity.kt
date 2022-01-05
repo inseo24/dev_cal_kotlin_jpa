98d1e7e0fb7b093abdfd335f8e075a14c8f5c928
seoin <befine324@gmail.com>
2022-01-05 16:14:46 +0900

    modify : AuditinEntityListner -> @CreationTimestamp, update~ annotation, remove JpaAuditConfig"

--- src/main/kotlin/com/example/dev_cal_kotlin_jpa/domain/BaseEntity.kt
+++ src/main/kotlin/com/example/dev_cal_kotlin_jpa/domain/BaseEntity.kt
@@ -1,24 +1,22 @@
 package com.example.dev_cal_kotlin_jpa.domain
 
-import org.springframework.data.annotation.CreatedDate
-import org.springframework.data.annotation.LastModifiedDate
-import org.springframework.data.jpa.domain.support.AuditingEntityListener
+import org.hibernate.annotations.CreationTimestamp
+import org.hibernate.annotations.UpdateTimestamp
 import java.time.LocalDateTime
 import javax.persistence.*
 
 @MappedSuperclass
-@EntityListeners(AuditingEntityListener::class)
 class BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long = 0
 
-    @CreatedDate
+    @CreationTimestamp
     @Column(name = "created_time", updatable = false)
     var createdTime: LocalDateTime? = null
 
-    @LastModifiedDate
+    @UpdateTimestamp
     @Column(name = "updated_time")
     var updatedTime: LocalDateTime? = null
 
