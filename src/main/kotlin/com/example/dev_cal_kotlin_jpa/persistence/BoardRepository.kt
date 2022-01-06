b0bcd3774d892a6b414fb621bbb23a5f464690a8
seoin <befine324@gmail.com>
2022-01-06 11:48:29 +0900

    add : EntityGraph in boardRepository(n+1)

--- src/main/kotlin/com/example/dev_cal_kotlin_jpa/persistence/BoardRepository.kt
+++ src/main/kotlin/com/example/dev_cal_kotlin_jpa/persistence/BoardRepository.kt
@@ -4,10 +4,14 @@ import com.example.dev_cal_kotlin_jpa.domain.Board
 import org.springframework.data.jpa.repository.EntityGraph
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
+import java.util.*
 
 @Repository
 interface BoardRepository : JpaRepository<Board, Long> {
 
-//    @EntityGraph(attributePaths = ["images", "comments"], type = EntityGraph.EntityGraphType.FETCH)
-//    override fun findAll() : MutableList<Board>
+    @EntityGraph(attributePaths = ["user"], type = EntityGraph.EntityGraphType.FETCH)
+    override fun findAll() : MutableList<Board>
+
+    @EntityGraph(attributePaths = ["user"], type = EntityGraph.EntityGraphType.FETCH)
+    override fun findById(id: Long): Optional<Board>
 }
